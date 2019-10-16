44c0663c2be307f7f957d66b5995bc32efb0dffd
Athina Barbul <athinabarbul@yahoo.com>
2019-10-16 11:23:59 +0300

    Added Edit button

--- online-shop/src/app/edit-product/edit-product.component.ts
+++ online-shop/src/app/edit-product/edit-product.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 import { ProductService } from '../product/service/product.service';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs';
@@ -13,15 +14,37 @@ import { ProductModel } from '../product/product-model';
 export class EditProductComponent implements OnInit {
 
   id: number;
-  product$: Observable<ProductModel>;
 
-  constructor(private route: ActivatedRoute, private productService: ProductService) {
+  listOfProductsObserv$: Observable<ProductModel[]>;
+  currentProduct: ProductModel = undefined;
+  listSubscription: Subscription;
+
+  constructor(private route: ActivatedRoute, private productService: ProductService,
+   private router: Router) {
 
   }
 
   ngOnInit() {
     this.id = parseInt(this.route.snapshot.paramMap.get('id'));
-    this.product$ = this.productService.getSpecificProduct(this.id);
+    this.listOfProductsObserv$ = this.productService.getProducts();
+
+
+    this.listSubscription = this.listOfProductsObserv$.subscribe(listOfProducts => {
+      for (let product of listOfProducts) {
+        if (product.id === this.id)
+          this.currentProduct = product;
+      }
+    })
+  }
+
+  goToProductDetails(i: number, currentProduct: ProductModel): void {
+    this.router.navigate(['/product/' + currentProduct.id]);
+  }
+
+  ngOnDestroy(): void{
+    if (this.listSubscription) {
+      this.listSubscription.unsubscribe();
+    }
   }
 
 }
